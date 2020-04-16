066acb66e1025702082570d75b05fd9b88daefec
Dariusz Kowalski <darekqa@gmail.com>
2020-04-16 13:02:28 +0100

    Further refactoring

--- ClearBank.DeveloperTest/Operations/PaymentOperations.cs
+++ ClearBank.DeveloperTest/Operations/PaymentOperations.cs
@@ -4,52 +4,38 @@
     {
         public static bool ExecutePayment(PaymentScheme scheme, Account account, MakePaymentRequest request)
         {
-            switch (scheme)
+            if (account != null)
             {
-                case PaymentScheme.Bacs:
-                    if (account == null)
-                    {
-                        return false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
-                    {
-                        return false;
-                    }
-                    break;
+                switch (scheme)
+                {
+                    case PaymentScheme.Bacs:
+                        if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
+                        {
+                            return false;
+                        }
+                        break;
 
-                case PaymentScheme.FasterPayments:
-                    if (account == null)
-                    {
-                        return false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
-                    {
-                        return false;
-                    }
-                    else if (account.Balance < request.Amount)
-                    {
-                        return false;
-                    }
-                    break;
+                    case PaymentScheme.FasterPayments:
+                        if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments) || account.Balance < request.Amount)
+                        {
+                            return false;
+                        }
+                        break;
 
-                case PaymentScheme.Chaps:
-                    if (account == null)
-                    {
+                    case PaymentScheme.Chaps:
+                        if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
+                        {
+                            return false;
+                        }
+                        else if (account.Status != AccountStatus.Live)
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
                         return false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
-                    {
-                        return false;
-                    }
-                    else if (account.Status != AccountStatus.Live)
-                    {
-                        return false;
-                    }
-                    break;
-                default:
-                    return false;
+                }
             }
-
             return false;
         }
     }
